2b9d1c5139fc423e768ad7834b4c823c7a1c0371
Alexander <vadersb@gmail.com>
2021-07-17 13:26:05 +0300

    initial project structure setup done

--- external_deps.cmake
+++ external_deps.cmake
@@ -1,6 +1,10 @@
-#RAYLIB_DIR to be specified
+# raylib 3.7.0 or later
+# RAYLIB_DIR to be specified
 if(NOT DEFINED RAYLIB_DIR)
     message(FATAL_ERROR "RAYLIB_DIR not defined!")
 endif()
 
 add_subdirectory(${RAYLIB_DIR} "raylib")
+
+# raygui
+# todo add later
